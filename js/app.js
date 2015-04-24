3846d9f6fa7b3df5ac11ca89cb64384e6e940331
Carlos Tacón <carlosjtacon@gmail.com>
2015-04-24 18:42:43 +0200

    Colores en %, Loading view, some custom css

--- js/app.js
+++ js/app.js
@@ -20,16 +20,46 @@ function parse_url(callback) {
 angular.module('bug', []) 
 
 .controller('BugControl', ['$scope','$http', function($scope, $http) {
+
+    $scope.getColor = function(rate) {
+      var percent_array = rate.split("%");
+      var percent = percent_array[0];
+      if(percent > 90) {
+        return 90;
+      } else if (percent > 80) {
+        return 80;
+      } else if (percent > 70) {
+        return 70;
+      } else if (percent > 60) {
+        return 60;
+      } else if (percent > 50) {
+        return 50;
+      } else if (percent > 40) {
+        return 40;
+      } else if (percent > 30) {
+        return 30;
+      } else if (percent > 20) {
+        return 20;
+      } else if (percent > 10) {
+        return 10;
+      } else if (percent > 0) {
+        return 0;
+      }
+    }
+
     parse_url(function(t_url) {
 
-		$scope.domain = "Cargando ...";
+        // $scope.domain = "Cargando ...";
+		$scope.domain = t_url;
 		console.log($scope.domain);
 		var url = 'https://www.kimonolabs.com/api/ondemand/cfie1m3k?apikey='+ api_key +'&kimpath2=' + t_url
 
 		$http.get(url).success(function(data){
 			console.log(url)
 			$scope.datos = data;
-            $scope.domain = t_url;
+            console.log(data)
+            // $scope.domain = t_url;
 		});
+
 	});
 }]);
