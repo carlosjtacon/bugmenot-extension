fdc40760d2486914081c55adb8a8796d9561ed2f
Mario Rios <mrm994@gmail.com>
2015-04-25 16:48:11 +0200

    bugfix
    fixed a bug that prevented the extension from loading, after closing a
    window or when working with multiple windows

--- Safari Extension/BugMeNot.safariextension/global.js
+++ Safari Extension/BugMeNot.safariextension/global.js
@@ -23,4 +23,14 @@ function handleToolBarEvent(event){
     var popover = safari.extension.createPopover("main",safari.extension.baseURI +"popover.html",300,400);
     item.popover = popover;
     item.showPopover();
+};
+
+/* deallocate the popover every time a new window/tap is activated
+*  this is done in order to avoid conflicts when working with more than one 
+*  window
+*/
+
+safari.application.addEventListener("activate",handleOpenEvent,true)
+function handleOpenEvent(event){
+    safari.extension.removePopover("main")
 }
